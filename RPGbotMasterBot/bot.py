741cb547c2c5a0e7e19f923c6789f05fb375a5d3
rrhaniery <r.rhaniery@gmail.com>
2025-04-20 15:28:16 -0300

    Update bot.py

--- RPGbotMasterBot/bot.py
+++ RPGbotMasterBot/bot.py
@@ -4,7 +4,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import random
 
-TOKEN = os.getenv("7572067124:AAF8lL8wW697hqZIRiPXQ3Nty_E-gbMb8Wo")
+TOKEN = os.getenv("BOT_TOKEN")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
